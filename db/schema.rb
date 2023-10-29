7af3aec8b65dd9e1ef62bc1c5795571e439a2454
Eki <ekiuzams@gmail.com>
2023-10-29 12:59:50 -0400

    Fixing database to add method column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_012037) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_29_165539) do
   create_table "events", force: :cascade do |t|
     t.string "event_type"
     t.date "event_date"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_012037) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "method"
   end
 
 end
