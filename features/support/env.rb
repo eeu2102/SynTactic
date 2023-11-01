138e24497ae3d40b93f17d5a6559366318ae96a3
markustrn <ht2573@columbia.edu>
2023-11-01 00:05:33 -0400

    SimpleCov implement

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@
 # newer version of cucumber-rails. Consider adding your own code to a new file
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
-
+require_relative '../../spec/simplecov_config'
 require 'cucumber/rails'
 
 # frozen_string_literal: true
