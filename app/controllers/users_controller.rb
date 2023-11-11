6c8595e36b5cc6b88274ee02ade9f351d88c42d5
Charles Liu <110317033+liucharlie213@users.noreply.github.com>
2023-11-11 14:53:13 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,11 @@
-# class UsersController < ApplicationController
-#   def new
-#     @user = User.new
-#   end
-
-#   def create
-#     @user = User.new(user_params)
-#     if @user.save
-#       # Handle a successful save, such as logging in the user, and redirecting to a profile page or dashboard.
-#     else
-#       # Re-render the 'new' form with error messages.
-#     end
-#   end
-
-#   private
-
-#   def user_params
-#     params.require(:user).permit(:username, :password, :email) # Add other fields as needed.
-#   end
-# end
-
 class UsersController < ApplicationController
-  # Skip before_action if you have set any that are not required for API requests
 
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save
-      session[:user_id] = @user.id # If you're using session-based authentication
-      render json: { id: @user.id, username: @user.username }, status: :created
-      # Or return a token if you're using token-based authentication
+      @user.generate_token # Generate a new token
+      render json: { id: @user.id, username: @user.username, token: @user.auth_token }, status: :created
     else
-      # Render error messages in JSON format
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
@@ -38,6 +13,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :email)
+    params.require(:user).permit(:username, :password)
   end
 end
