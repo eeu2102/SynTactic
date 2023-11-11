75a94ebd0b17dfc41298ae9a14290cfe0f231797
Eki <ekiuzams@gmail.com>
2023-11-11 14:45:12 -0500

    Add auth_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_10_030854) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_11_194447) do
   create_table "events", force: :cascade do |t|
     t.string "event_type"
     t.date "event_date"
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_030854) do
     t.datetime "updated_at", null: false
     t.string "progress"
     t.string "language"
+    t.string "auth_token"
   end
 
 end
